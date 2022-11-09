5302cf9cc6007824b02cd9d944a8748793cff09f
luisbmonroyj <lbmonroyj@gmail.com>
2022-11-09 18:16:24 -0500

    models created

---
+++ src/test/java/backend/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
